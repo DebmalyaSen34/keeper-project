1a588a14e0384bdb20e0fd4b55465fa586e5eb52
DebmalyaSen1519 <ds22btb0a15@student.nitw.ac.in>
2024-06-04 13:21:05 +0530

    Used mapping to add more notes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import './App.css';
+import notes from './notes';
+
+function createNote(note){
+  return (
+    <Note 
+    key={note.key}
+    title={note.title}
+    content={note.content} />
+  );
+};
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
